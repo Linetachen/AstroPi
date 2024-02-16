80dcce43de7643ce879b46ab909ff3385ccb8a2d
Sam <95887362+SamuelMaru@users.noreply.github.com>
2024-02-16 18:44:54 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,5 @@
-# Team: Piberry, Authors: Erik, ...
-
+# Team: Piberry, Authors: Erik, Sam
 from picamera import PiCamera
-from sense_hat import SenseHat
 from time import sleep
 from datetime import datetime
 from pathlib import Path
@@ -10,15 +8,15 @@ from exif import Image
 import numpy as np
 import cv2
 import os
-import shutil
+import SensorData
 
 # constants
 ROOT_FOLDER = (Path(__file__).parent).resolve()
 LOG_FILE = ROOT_FOLDER / "AstroPi.log"
 RESOLUTION = (4056, 3040) # resolution of the Pi camera
-CALCULATION_TIME = 570 #seconds, time allowed for speed calculation
+CALCULATION_TIME = 570 #seconds, time allowed for speed calculation # MEANT TO BE 570
 GSD = 0.1243 # Ground Sampling Distance, km per pixel
-INTERVAL = 2 #seconds
+INTERVAL = 3 #seconds 
 
 def getTime(imagePath):
     """Get the time the image was taken from the exif data of the image."""
@@ -45,24 +43,7 @@ def writeMeanSpeed(meanSpeed):
         speed = speed[:min(len(speed),6)]
         speedFile.write(speed)
 
-def logSensorData(senseHat):
-    """Log the sensor data to the log file."""
-    # get sensor data
-    orientation = senseHat.get_orientation()
-    compassNorth = senseHat.get_compass()
-    magnetometer = senseHat.get_compass_raw()
-    gyroscope = senseHat.get_gyroscope_raw()
-    accelerometer = senseHat.get_accelerometer_raw()
-    humidity = senseHat.get_humidity()
-    temperature = senseHat.get_temperature()
-    pressure = senseHat.get_pressure()
-    # log data
-    logger.info(f"Data:{datetime.now().strftime('%Y-%m-%d %H:%M:%S:%f')},{orientation['yaw']},{orientation['pitch']},{orientation['roll']},{compassNorth},{magnetometer['x']},{magnetometer['y']},{magnetometer['z']},{gyroscope['x']},{gyroscope['y']},{gyroscope['z']},{accelerometer['x']},{accelerometer['y']},{accelerometer['z']},{humidity},{temperature},{pressure}")
-
-def main():
-    # sense
-    senseHat = SenseHat()
-
+def main():    
     # setup camera
     camera = PiCamera()
     camera.resolution = RESOLUTION
@@ -73,36 +54,40 @@ def main():
 
     # capture images and calculate speed within the runtime
     while (datetime.now() - startTime).total_seconds() < CALCULATION_TIME:
-        try:
-            # log sensor data
-            logSensorData(senseHat)
-            # capture image
-            currentImagePath = ROOT_FOLDER / f"image{len(imagePaths)}.jpg"
-            camera.capture(str(currentImagePath))
-            imagePaths.append(currentImagePath)
+        current_image_path = ROOT_FOLDER / f"image{len(imagePaths)}.jpg"
+        if SensorData.space_remaining(ROOT_FOLDER):
+            camera.capture(str(current_image_path))
+            imagePaths.append(current_image_path)
+            logger.info(f"Successfully took image {len(imagePaths)}.")
+        else:
+            logger.warning(SensorData.space_remaining(ROOT_FOLDER))
+            logger.warning("Unable to store image, capacity exceeded")
 
-            # calculate speed if there are at least two images
-            if len(imagePaths) > 2:
-                # consider last two images for speed calculation
-                oldStrPath = str(imagePaths[-2])
-                oldImage = cv2.imread(oldStrPath)
-                oldImageTime = getTime(oldStrPath)
-                newStrPath = str(imagePaths[-1])
-                newImage = cv2.imread(newStrPath)
-                newImageTime = getTime(newStrPath)
-                timeDelta = (newImageTime - oldImageTime).total_seconds()
+        # calculate speed if there are at least two images
+        if len(imagePaths) > 2:
+            # consider last two images for speed calculation
+            oldStrPath = str(imagePaths[-2])
+            oldImage = cv2.imread(oldStrPath)
+            oldImageTime = getTime(oldStrPath)
+            newStrPath = str(imagePaths[-1])
+            newImage = cv2.imread(newStrPath)
+            newImageTime = getTime(newStrPath)
+            timeDelta = (newImageTime - oldImageTime).total_seconds()
 
-                # compute matches and keypoints using cv2
-                matches, kp1, kp2 = getMatchesAndKeyPoints(oldImage, newImage)
+            # compute matches and keypoints using cv2
+            matches, kp1, kp2 = getMatchesAndKeyPoints(oldImage, newImage)
 
-                # calculate speed if there are matches
-                if len(matches) > 0:
-                    speed = distanceFromMatches(matches, kp1, kp2)*GSD/timeDelta
+            # calculate speed if there are matches
+            if len(matches) > 0:
+                speed = distanceFromMatches(matches, kp1, kp2)*GSD/timeDelta
+                if(6.3<=speed<=8.9):
                     meanSpeed = (meanSpeed*speedCalcCounter + speed)/(speedCalcCounter+1)
                     speedCalcCounter += 1
                     logger.info(f"Speed: {speed} km/s, Mean speed: {meanSpeed} km/s")
-        except Exception as e:
-            logger.error(e)
+                else:
+                    logger.info(f"Speed {speed} km/s was too high/low and rejected as an outlier")
+        if speedCalcCounter%3==0:
+            SensorData.dump_sensor_data(ROOT_FOLDER,current_image_path)
         sleep(INTERVAL) # wait before taking next image
 
     camera.close()
@@ -111,7 +96,7 @@ def main():
     imageKeepInterval = np.ceil(len(imagePaths)/40)
     for i in range(len(imagePaths)):
         if i % imageKeepInterval != 0:
-            os.remove(str(imagePaths[i]))        
+            os.remove(str(imagePaths[i])) 
 
     # write mean speed to file
     writeMeanSpeed(meanSpeed)
@@ -128,6 +113,3 @@ if __name__ == "__main__":
         # raise e # uncomment to rethrow the exception. Useful when debugging.
     finally:
         logger.info("Program ended.")
-
-
-    
